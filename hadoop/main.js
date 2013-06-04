e3eba61b241e463eef9211a45c1c90e0e1b85cbb
byeah <jby.yeah@gmail.com>
2013-06-04 19:41:05 +0800

    New pages

--- hadoop/main.js
+++ hadoop/main.js
@@ -20,16 +20,20 @@
 		return [{id:d.id,type:getType(d.id),slot:d.slot,len:d.events[1].time-d.events[0].time,time:d.events[0].time,ison:0}]
 	}
 
+
 	function display(data)
 	{
 		var blocks=data.map(getEvent).reduce(function(a,b){return a.concat(b)})
-		var maxLen=d3.max(blocks.filter(function(d){return d.id.indexOf("_m_")>=0}).map(function(d){return d.len}))
+		var maxLen=d3.max(blocks.map(function(d){return d.len}))
+		var maxMapLen=d3.max(blocks.filter(function(d){return d.id.indexOf("_m_")>=0}).map(function(d){return d.len}))
+		var maxReduceLen=d3.max(blocks.filter(function(d){return d.id.indexOf("_r_")>=0}).map(function(d){return d.len}))
 		var maxTime=d3.max(blocks.map(function(d){return d.time+d.len}))
 		var chart = d3.select("body").append("svg")
 			.attr("class", "chart")
 			.attr("width", blocks.length*blockWidth*10)
 			.attr("height", height)
-		var x=d3.scale.linear().domain([0,maxLen]).range([0,blockWidth])
+		var curLen=d3.scale.linear().range([0,maxTime]).domain([0,1024])(blockWidth)
+		var x=d3.scale.linear().domain([0,curLen]).range([0,blockWidth])
 		var selected=0
 		var svg=chart.selectAll("rect")
 			.data(blocks)
@@ -38,7 +42,7 @@
 			.attr("y", function(d){return (maxSlot*(d.id.indexOf("_rr_")>=0)+d.slot)*blockHeight+margin.top+0.5})
 			.attr("width", function(d){return x(d.len)})
 			.attr("height", function(d) { return blockHeight-0.5 })
-			.attr("fill",function(d){return colors[d.type]})
+			.attr("fill",function(d){return getColor(d)})
 			.on("mouseover",function(d){d.ison=1;selected=d;redraw(curLen)})
 			.on("mouseout",function(d,i){blocks[i].ison=0;selected=0;redraw(curLen)})
 
@@ -57,20 +61,20 @@
 			.attr("transform", "translate("+margin.left+"," + (height - padding) + ")")
 			.call(xAxis)
 		var line = [chart.append("line"),chart.append("line")]
-		var curLen=maxLen
 		d3.select("body").on("mousewheel",function()
 							 {
 								 var delta=curLen*(event.wheelDelta/120)*0.15
 								 if (Math.abs(delta)<2)
 									 delta=event.wheelDelta/60
-								 if (curLen+delta<3)return
-								 curLen+=delta
+								 if (curLen-delta<3)return
+								 curLen+=-delta
 								 x=d3.scale.linear().domain([0,curLen]).range([0,blockWidth])
 								 redraw(curLen)
 							})
 
 		function redraw(curLen)
 		{
+			//filter(function(d){return d.id.indexOf("_m_")>=0})	
 			var x=d3.scale.linear().domain([0,curLen]).range([0,blockWidth])
 			var xAxis = d3.svg.axis().scale(d3.scale.linear().domain([0,maxTime]).range([0,x(maxTime)])).orient("bottom");
 			if (selected==0)
@@ -97,7 +101,7 @@
 				.attr("y", function(d){return (maxSlot*(d.id.indexOf("_rr_")>=0)+d.slot)*blockHeight+margin.top+0.5})
 				.attr("width", function(d){return x(d.len)})
 				.attr("height", function(d) { return blockHeight-0.5 })
-				.attr("fill",function(d){return colors[d.type]})
+				.attr("fill",function(d){return getColor(d)})
 				.attr("opacity",function(d){return 1-0.5*d.ison})
 /*			chart.selectAll("text")
 				.data(blocks)
@@ -108,8 +112,24 @@
 				.text(function(d){if (x(d.len)>30) return d.len+"s"; else return "";})
 				.attr("dx", function(d){return 3})
 				.attr("dy", function(d) { return ".35em" })
-*/		}
+*/	
+
+		}
+
+		function getColor(d)
+		{
+//			console.log(d.type,d.len,maxMapLen,maxReduceLen)
+			if (d.type==0)
+			{
+			//	console.log((255-d.len/maxMapLen*255))
+//				console.log("rgb(255,51,"+int(255-d.len/maxMapLen*255)+")")
+				return "rgb(255,51,"+Math.round(255-d.len/maxMapLen*255)+")"
+			}
+			else
+				return "rgb(0,51,"+Math.round(d3.scale.linear().domain([0,maxReduceLen]).range([255,153])(d.len))+")"
+		}
 	}
+
 	exports.display=display
 })(this)
 display(jsondata)
